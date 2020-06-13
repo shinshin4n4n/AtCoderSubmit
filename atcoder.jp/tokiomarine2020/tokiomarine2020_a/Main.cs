14b1ece56a403617c7fd1d87bd350727d8fed34d
shinshin4n4n <s.iguchi777@gmail.com>
2020-06-13 22:05:00 +0900

    [AC] tokiomarine2020 tokiomarine2020_a

---
+++ atcoder.jp/tokiomarine2020/tokiomarine2020_a/Main.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Globalization;
+
+namespace AtCoder_Kaijou
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string s1 = input.Substring(0, 3);
+
+         Console.WriteLine(s1);
+        }
+    }
+}
