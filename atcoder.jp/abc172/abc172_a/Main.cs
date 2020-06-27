2aa8f597e3acc780d9ffac189f52ba54b855855b
shinshin4n4n <s.iguchi777@gmail.com>
2020-06-27 21:01:32 +0900

    [AC] abc172 abc172_a

---
+++ atcoder.jp/abc172/abc172_a/Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace AtCoder0627
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a + a*a + a*a*a);
+        }
+    }
+}
