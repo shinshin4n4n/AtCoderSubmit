a2e60bd4746ce7e1ce9b5ccd4de93a4f599e73a7
shinshin4n4n <s.iguchi777@gmail.com>
2020-09-13 21:01:37 +0900

    [AC] abc178 abc178_a

---
+++ atcoder.jp/abc178/abc178_a/Main.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AtCoder0913
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Na = int.Parse(Console.ReadLine());
+            //int[] N = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray() ;
+
+            if (Na == 0)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
