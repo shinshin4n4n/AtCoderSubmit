eb3faeb1263cb103aea778421359f61b4ef82388
shinshin4n4n <s.iguchi777@gmail.com>
2020-09-27 08:18:46 +0900

    [AC] abc166 abc166_a

---
+++ atcoder.jp/abc166/abc166_a/Main.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace AtCoderPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            if (S == "ABC")
+            {
+                Console.WriteLine("ARC");
+            }
+            else
+            {
+                Console.WriteLine("ABC");
+            }
+        }
+    }
+}
