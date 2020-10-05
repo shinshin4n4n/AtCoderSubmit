675e5cffb539b30a32f7bb64aaf7b8fa83fcbeb4
shinshin4n4n <s.iguchi777@gmail.com>
2020-10-05 17:34:07 +0900

    [AC] abc151 abc151_a

---
+++ atcoder.jp/abc151/abc151_a/Main.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+
+class AtCoder
+{
+    public static void Main(string[] args)
+    {
+        char c = Console.ReadLine().ToCharArray()[0];
+        char ans = (char)((int)c + 1);
+
+        Console.WriteLine(ans.ToString());
+
+    }
+}
