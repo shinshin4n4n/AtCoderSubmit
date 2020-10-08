504ae22f72f70a6ebd0565e874d31b0ed0053265
shinshin4n4n <s.iguchi777@gmail.com>
2020-10-08 17:53:03 +0900

    [AC] abc089 abc089_a

---
+++ atcoder.jp/abc089/abc089_a/Main.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(N/3);
+
+        }
+    }
+}
