f7b7e104c2cf4bcce1d4e10e1ab423b259dd0679
shinshin4n4n <s.iguchi777@gmail.com>
2020-10-12 09:14:48 +0900

    [AC] abc134 abc134_a

---
+++ atcoder.jp/abc134/abc134_a/Main.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = int.Parse(Console.ReadLine());
+            Console.WriteLine(3 * r * r);
+
+        }
+    }
+}
