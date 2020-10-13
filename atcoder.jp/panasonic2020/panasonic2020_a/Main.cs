f90bf46a8e3b5a950e6cb0584c50b23f39ddae55
shinshin4n4n <s.iguchi777@gmail.com>
2020-10-13 15:23:18 +0900

    [AC] panasonic2020 panasonic2020_a

---
+++ atcoder.jp/panasonic2020/panasonic2020_a/Main.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int K = int.Parse(Console.ReadLine());
+            int[] a = {1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51};
+
+            Console.WriteLine(a[K-1]);
+        }
+    }
+}
