dc0ae6c7fc86c6f153bb664336b17237c45e7af4
shinshin4n4n <s.iguchi777@gmail.com>
2020-10-19 13:26:48 +0900

    [AC] abc090 abc090_a

---
+++ atcoder.jp/abc090/abc090_a/Main.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string c1 = Console.ReadLine();
+        string c2 = Console.ReadLine();
+        string c3 = Console.ReadLine();
+
+        Console.Write(c1[0]);
+        Console.Write(c2[1]);
+        Console.Write(c3[2]);
+    }        
+    }
