78f3b8133d363de2fd63589aa3c22b2155eaaa7d
shinshin4n4n <s.iguchi777@gmail.com>
2020-10-23 15:53:31 +0900

    [AC] abc154 abc154_b

---
+++ atcoder.jp/abc154/abc154_b/Main.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string S = Console.ReadLine();
+
+        for(var i = 0; i< S.Length; i++)
+        {
+            Console.Write('x');
+        }
+
+    }
+}
